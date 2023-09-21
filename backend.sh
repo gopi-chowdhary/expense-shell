aa593b9cf8b1ea81999220b69fbc31833480f525
gopi chowdhary <gopichowdhary99@gmail.com>
2023-09-21 13:23:24 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -3,17 +3,23 @@ component=backend
 
 echo Install Nodejs Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+echo $?
+
 echo Install NodeJs
 dnf install nodejs -y &>>$log_file
+echo $?
 
 echo Copy Backend Service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 
 echo Add application user
 useradd expense &>>$log_file
+echo $?
 
 echo clean app content
 rm -rf /app &>>$log_file
+echo $?
 mkdir /app
 cd /app
 
@@ -21,14 +27,18 @@ download_and_extract
 
 echo download dependencies
 npm install &>>$log_file
+echo $?
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
-systemctl start backend &>>$log_file
+systemctl restart backend &>>$log_file
+echo $?
 
 echo install mysql client
 dnf install mysql -y &>>$log_file
+echo $?
 
 echo load schema
 mysql -h <mysql.vdevops69.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
