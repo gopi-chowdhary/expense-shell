16310915d8039d37205ed344391bf94c0bf4a48f
gopi chowdhary <gopichowdhary99@gmail.com>
2023-09-21 11:42:34 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -1,21 +1,37 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-dnf install nodejs -y
+source common.sh
 
-cp backend.service /etc/systemd/system/backend.service
+echo Install Nodejs Repos
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+echo Install NodeJs
+dnf install nodejs -y >>$log_file
 
-useradd expense
-rm -rf /app
+echo Copy Backend Service file
+cp backend.service /etc/systemd/system/backend.service >>$log_file
+
+echo Add application user
+useradd expense >>$log_file
+
+echo clean app content
+rm -rf /app >>$log_file
 mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+
+echo Download app content
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
+
 cd /app
-unzip /tmp/backend.zip
+echo Extract app content
+unzip /tmp/backend.zip >>$log_file
 
-npm install
+echo download dependencies
+npm install >>$log_file
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+echo start backend service
+systemctl daemon-reload >>$log_file
+systemctl enable backend >>$log_file
+systemctl start backend >>$log_file
 
-dnf install mysql -y
+echo install mysql client
+dnf install mysql -y >>$log_file
 
-mysql -h <mysql.vdevops69.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo load schema
+mysql -h <mysql.vdevops69.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,19 @@
-dnf module disable mysql -y
+source common.sh
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo disable mysql 8 version
+dnf module disable mysql -y >>$log_file
 
-dnf install mysql-community-server -y
+echo copy mysql repo file
+cp mysql.repo /etc/yum.repos.d/mysql.repo >>$log_file
 
-systemctl enable mysqld
-systemctl start mysqld
+echo install mysql server
+dnf install mysql-community-server -y >>$log_file
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo start mysql service
+systemctl enable mysqld >>$log_file
+systemctl start mysqld >>$log_file
+
+echo setup root password
+mysql_secure_installation --set-root-pass ExpenseApp@1 >>$log_file
 
 
