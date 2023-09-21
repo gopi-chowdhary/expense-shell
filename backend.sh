19ff187631660e072ccbb74503ea32b6687a6959
gopi chowdhary <gopichowdhary99@gmail.com>
2023-09-21 12:42:29 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -2,33 +2,33 @@ source common.sh
 component=backend
 
 echo Install Nodejs Repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 echo Install NodeJs
-dnf install nodejs -y >>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo Copy Backend Service file
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo Add application user
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo clean app content
-rm -rf /app >>$log_file
+rm -rf /app &>>$log_file
 mkdir /app
 cd /app
 
 download_and_extract
 
 echo download dependencies
-npm install >>$log_file
+npm install &>>$log_file
 
 echo start backend service
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo install mysql client
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
 echo load schema
-mysql -h <mysql.vdevops69.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h <mysql.vdevops69.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
--- frontend.sh
+++ frontend.sh
@@ -2,18 +2,18 @@ source common.sh
 component=frontend
 
 echo Installing Nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_file
 
 echo Placing Expense config File In Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo Removing Old Nginx Content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 cd /usr/share/nginx/html
 
 download_and_extract
 
 echo Starting Nginx Service
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
